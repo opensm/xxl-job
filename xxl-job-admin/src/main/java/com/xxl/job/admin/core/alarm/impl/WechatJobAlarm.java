a65ab94eb0a60be92c96bb99184af548e72836cb
git <smxblysq@qq.com>
2023-06-09 15:12:57 +0800

    修复关于显示的bugs

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/alarm/impl/WechatJobAlarm.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/alarm/impl/WechatJobAlarm.java
@@ -50,7 +50,7 @@ public class WechatJobAlarm implements JobAlarm {
             if (null != msg && !"".equals(msg.trim())) {
                 logger.info("当前请求wechat消息内容为：" + msg);
                 // msg = msg.substring(msg.lastIndexOf("</span><br>") + 11, msg.lastIndexOf("<br><br>"));
-                msg = msg.replaceAll("<br>","\\n");
+                msg = msg.replaceAll("<br>","\n");
                 msg = msg.replaceAll("<span .*>","<font color=\"warning\">");
                 msg = msg.replaceAll("</span>","</font>");
                 content.append("\n> " + msg);
